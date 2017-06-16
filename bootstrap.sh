dfbc2fe7c537eeaf34a58c37a00c6277f48ab1bd
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2017-06-16 16:19:08 +0530

    bootstrap.sh: corrected bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Python 3.5
-sudo yum install python35{,-pip}
+sudo yum -y install python35{,-{devel,pip}} htop
 
 # LogiX
 mkdir -p /home/hadoop/wheels/
 aws s3 cp s3://done-vyasa/conf/logix-0.1.0-py3-none-any.whl \
     /home/hadoop/wheels/
+aws s3 cp s3://done-vyasa/conf/python-site-packages.tar.gz \
+    /home/hadoop/wheels/
 
+tar -xzf /home/hadoop/wheels/python-site-packages.tar.gz
 pip-3.5 install --user /home/hadoop/wheels/logix-0.1.0-py3-none-any.whl
 
 # access
